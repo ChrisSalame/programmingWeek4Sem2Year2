07d45ffc89248f7081497865406a17e8b2096d6c
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-02-05 18:13:55 -0500

    testingLerp

--- Assets/Scripts/shrinkAndGrowAT.cs
+++ Assets/Scripts/shrinkAndGrowAT.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 
 namespace NodeCanvas.Tasks.Actions {
 
+
 	public class shrinkAndGrowAT : ActionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
-            agent.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+
+        Vector3 baseSize = new Vector3(2f, 2f, 2f);
+        Vector3 smallSize = new Vector3(0.5f, 0.5f, 0.5f);
+
+        protected override string OnInit() {
+
+
+            agent.transform.localScale = new Vector3.Lerp(baseSize,smallSize, 2);
             return null;
 		}
 
@@ -19,7 +26,9 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-            agent.transform.localScale += new Vector3(2f, 2f, 2f);
+
+
+            //agent.transform.localScale = new Vector3.Lerp(baseSize, smallSize, 2);
 
             EndAction(true);
 		}
