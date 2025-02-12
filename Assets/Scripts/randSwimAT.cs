7a20f241175c43a7a799f8831a18121efd5592f8
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-02-12 10:10:04 -0500

    testingBlackboard

--- Assets/Scripts/randSwimAT.cs
+++ Assets/Scripts/randSwimAT.cs
@@ -11,6 +11,9 @@ namespace NodeCanvas.Tasks.Actions {
         public float searchRadius;
         private NavMeshAgent navAgent;
 
+        public BBParameter<float> energy;
+        public BBParameter<float> energyUsage;
+
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -26,6 +29,10 @@ namespace NodeCanvas.Tasks.Actions {
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
 
+            Blackboard energyBlackboard = agent.GetComponent<Blackboard>();
+            energy = agentBlackboard.GetVeriableValue<float>("energy");
+            
+
             Vector3 swimTarget = Random.insideUnitSphere * searchRadius + agent.transform.position;
             NavMeshHit navMeshHit;
             if (!NavMesh.SamplePosition (swimTarget, out navMeshHit, searchRadius, NavMesh.AllAreas))
@@ -38,6 +45,8 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
+
+            energy.value -= energyUsage.value * Time.deltaTime;
 
             if (navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)
             {
